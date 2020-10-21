57112683061dc2103e64eee159530be32b9e2df2
Bryce Wilson <bryce.wilson@alumni.ubc.ca>
2020-10-20 21:16:44 -0700

    Fix rbg vx rgb issue

--- commands.py
+++ commands.py
@@ -161,7 +161,7 @@ class Main(commands.Cog):
 
         css = {"lightsalmon": "0xFFA07A", "salmon": "0xFA8072", "darksalmon": "0xE9967A", "lightcoral": "0xF08080", "indianred": "0xCD5C5C", "crimson": "0xDC143C", "firebrick": "0xB22222", "red": "0xFF0000", "darkred": "0x8B0000", "coral": "0xFF7F50", "tomato": "0xFF6347", "orangered": "0xFF4500", "gold": "0xFFD700", "orange": "0xFFA500", "darkorange": "0xFF8C00", "lightyellow": "0xFFFFE0", "lemonchiffon": "0xFFFACD", "lightgoldenrodyellow": "0xFAFAD2", "papayawhip": "0xFFEFD5", "moccasin": "0xFFE4B5", "peachpuff": "0xFFDAB9", "palegoldenrod": "0xEEE8AA", "khaki": "0xF0E68C", "darkkhaki": "0xBDB76B", "yellow": "0xFFFF00", "lawngreen": "0x7CFC00", "chartreuse": "0x7FFF00", "limegreen": "0x32CD32", "lime": "0x00FF00", "forestgreen": "0x228B22", "green": "0x008000", "darkgreen": "0x006400", "greenyellow": "0xADFF2F", "yellowgreen": "0x9ACD32", "springgreen": "0x00FF7F", "mediumspringgreen": "0x00FA9A", "lightgreen": "0x90EE90", "palegreen": "0x98FB98", "darkseagreen": "0x8FBC8F", "mediumseagreen": "0x3CB371", "seagreen": "0x2E8B57", "olive": "0x808000", "darkolivegreen": "0x556B2F", "olivedrab": "0x6B8E23", "lightcyan": "0xE0FFFF", "cyan": "0x00FFFF", "aqua": "0x00FFFF", "aquamarine": "0x7FFFD4", "mediumaquamarine": "0x66CDAA", "paleturquoise": "0xAFEEEE", "turquoise": "0x40E0D0", "mediumturquoise": "0x48D1CC", "darkturquoise": "0x00CED1", "lightseagreen": "0x20B2AA", "cadetblue": "0x5F9EA0", "darkcyan": "0x008B8B", "teal": "0x008080", "powderblue": "0xB0E0E6", "lightblue": "0xADD8E6", "lightskyblue": "0x87CEFA", "skyblue": "0x87CEEB", "deepskyblue": "0x00BFFF", "lightsteelblue": "0xB0C4DE", "dodgerblue": "0x1E90FF", "cornflowerblue": "0x6495ED", "steelblue": "0x4682B4", "royalblue": "0x4169E1", "blue": "0x0000FF", "mediumblue": "0x0000CD", "darkblue": "0x00008B", "navy": "0x000080", "midnightblue": "0x191970", "mediumslateblue": "0x7B68EE", "slateblue": "0x6A5ACD", "darkslateblue": "0x483D8B", "lavender": "0xE6E6FA", "thistle": "0xD8BFD8", "plum": "0xDDA0DD", "violet": "0xEE82EE", "orchid": "0xDA70D6", "fuchsia": "0xFF00FF", "magenta": "0xFF00FF", "mediumorchid": "0xBA55D3", "mediumpurple": "0x9370DB", "blueviolet": "0x8A2BE2", "darkviolet": "0x9400D3", "darkorchid": "0x9932CC", "darkmagenta": "0x8B008B", "purple": "0x800080", "indigo": "0x4B0082", "pink": "0xFFC0CB", "lightpink": "0xFFB6C1", "hotpink": "0xFF69B4", "deeppink": "0xFF1493", "palevioletred": "0xDB7093", "mediumvioletred": "0xC71585", "white": "0xFFFFFF", "snow": "0xFFFAFA", "honeydew": "0xF0FFF0", "mintcream": "0xF5FFFA", "azure": "0xF0FFFF", "aliceblue": "0xF0F8FF", "ghostwhite": "0xF8F8FF", "whitesmoke": "0xF5F5F5", "seashell": "0xFFF5EE", "beige": "0xF5F5DC", "oldlace": "0xFDF5E6", "floralwhite": "0xFFFAF0", "ivory": "0xFFFFF0", "antiquewhite": "0xFAEBD7", "linen": "0xFAF0E6", "lavenderblush": "0xFFF0F5", "mistyrose": "0xFFE4E1", "gainsboro": "0xDCDCDC", "lightgray": "0xD3D3D3", "silver": "0xC0C0C0", "darkgray": "0xA9A9A9", "gray": "0x808080", "dimgray": "0x696969", "lightslategray": "0x778899", "slategray": "0x708090", "darkslategray": "0x2F4F4F", "black": "0x000000", "cornsilk": "0xFFF8DC", "blanchedalmond": "0xFFEBCD", "bisque": "0xFFE4C4", "navajowhite": "0xFFDEAD", "wheat": "0xF5DEB3", "burlywood": "0xDEB887", "tan": "0xD2B48C", "rosybrown": "0xBC8F8F", "sandybrown": "0xF4A460", "goldenrod": "0xDAA520", "peru": "0xCD853F", "chocolate": "0xD2691E", "saddlebrown": "0x8B4513", "sienna": "0xA0522D", "brown": "0xA52A2A", "maroon": "0x800000"}
 
-        def color_embed(colour, r, b, g, c, m, y, k, h, s, l):
+        def color_embed(colour, r, g, b, c, m, y, k, h, s, l):
             Hex = f"#{RGB_to_HEX(r, g, b)}"
             rgb = f"rgb({r},{g},{b})"
             hsl = f"hsl({round(float(h), 2)},{round(float(s), 2)}%,{round(float(l), 2)}%)"
@@ -179,7 +179,7 @@ class Main(commands.Cog):
         def RGB(r, g, b, colour):
             h, s, l = RGB_to_HSL(r, g, b)
             c, m, y, k = RGB_to_CMYK(r, g, b)
-            return color_embed(colour, r, b, g, c, m, y, k, h, s, l)
+            return color_embed(colour, r, g, b, c, m, y, k, h, s, l)
 
         def hslRGB(h, s, l, colour):
             h -= (h // 360) * 360
@@ -188,7 +188,8 @@ class Main(commands.Cog):
             r = round(r)
             g = round(g)
             b = round(b)
-            return color_embed(colour, r, b, g, c, m, y, k, h, s, l)
+            return color_embed(colour, 
+                               , c, m, y, k, h, s, l)
 
         def cmykRGB(c, m, y, k, colour):
             r, g, b = CMYK_to_RGB(c, m, y, k)
@@ -196,7 +197,7 @@ class Main(commands.Cog):
             r = round(r)
             g = round(g)
             b = round(b)
-            return color_embed(colour, r, b, g, c, m, y, k, h, s, l)
+            return color_embed(colour, r, g, b, c, m, y, k, h, s, l)
 
         def cssRGB(colour):
             r = int(css[colour][2:4], 16)
@@ -204,7 +205,7 @@ class Main(commands.Cog):
             b = int(css[colour][6:], 16)
             h, s, l = RGB_to_HSL(r, g, b)
             c, m, y, k = RGB_to_CMYK(r, g, b)
-            return color_embed(colour, r, b, g, c, m, y, k, h, s, l)
+            return color_embed(colour, r, g, b, c, m, y, k, h, s, l)
 
         colour = ctx.message.content[len(self.bot.command_prefix) + 7:]
 
@@ -223,10 +224,10 @@ class Main(commands.Cog):
             elif len(c_str) == 3:
                 await ctx.send(embed=RGB(int(c_str[0] * 2, 16), int(c_str[1] * 2, 16), int(c_str[2] * 2, 16), c_str))
         elif c_str := re.search(r"rgb\((\d{1,3}), *(\d{1,3}), *(\d{1,3})\)", colour):
-            r, b, g = map(int, c_str.group(1, 2, 3))
+            r, g, b = map(int, c_str.group(1, 2, 3))
             if max(r, g, b) > 255 or min(r, g, b) < 0:
                 return await ctx.send("You inputted an invalid colour. Please try again.", delete_after=5)
-            await ctx.send(embed=RGB(r, b, g, c_str))
+            await ctx.send(embed=RGB(r, g, b, c_str))
         elif c_str := re.search(r"hsl\((\d{1,3}(?:\.\d*)?), *(\d{1,3}(?:\.\d*)?)%?, *(\d{1,3}(?:\.\d*)?)%?\)", colour):
             h, s, l = map(Fraction, c_str.group(1, 2, 3))
             if h > 360 or max(s, l) > 100 or min(h, s, l) < 0:
