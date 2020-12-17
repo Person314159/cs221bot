6a22cf394b6743bc33d8105f0eb2cd6deeeac396
echau01 <echau01@students.cs.ubc.ca>
2020-12-17 02:40:37 -0800

    Insert blank line after docstring

--- util/create_file.py
+++ util/create_file.py
@@ -7,6 +7,7 @@ def create_file_if_not_exists(file_path):
     Creates file with given path (as str) if the file does not already exist.
     All required directories are created, too.
     """
+    
     pathlib.Path(os.path.dirname(file_path)).mkdir(parents=True, exist_ok=True)
     with open(file_path, 'a'):
         pass
