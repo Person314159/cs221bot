d50575054e2b70c2a67ea5172ab7bd64e64896d1
William Zhang <zhangwil@students.cs.ubc.ca>
2021-01-19 16:26:54 -0800

    change pread follow up comments text

--- cogs/piazza.py
+++ cogs/piazza.py
@@ -186,7 +186,7 @@ class Piazza(commands.Cog):
 
             post_embed.set_thumbnail(url=PIAZZA_THUMBNAIL_URL)
 
-            post_embed.add_field(name=f"{post['num_answers']} more contributions hidden", value="Click the title above to access the rest of the post.", inline=False)
+            post_embed.add_field(name=f"{post['num_answers']} followup comments hidden", value="Click the title above to access the rest of the post.", inline=False)
 
             post_embed.set_footer(text=f"tags: {post['tags']}")
             return post_embed
