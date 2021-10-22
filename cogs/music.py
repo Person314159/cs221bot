5d6e0dd0e7d5a4abc40b8b7a96656223025aee38
Person314159 <williamzy314@gmail.com>
2021-10-22 12:42:09 -0700

    ffmpeg bs

--- cogs/music.py
+++ cogs/music.py
@@ -54,6 +54,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
     FFMPEG_OPTIONS = {
         "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
         "options": "-vn",
+        "executable": "C:/FFmpeg/bin/ffmpeg.exe"
     }
 
     ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
